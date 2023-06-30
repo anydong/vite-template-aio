438b4747f204bc95de15232f72389c8b7cf90371
liuzhaowei55 <liuzhaowei55@gmail.com>
2023-06-30 13:38:25 +0800

    feat: locale

--- src/config/locale.ts
+++ src/config/locale.ts
@@ -1,7 +1,5 @@
 import { InitOptions } from "i18next";
 
-export const defaultLocale = "zh-Hans";
-
 export const initOptions: InitOptions = {
   backend: {
     // 網頁載入時去下載語言檔的位置
@@ -11,10 +9,10 @@ export const initOptions: InitOptions = {
   // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
   // if you're using a language detector, do not define the lng option
   fallbackLng: {
-    default: ["en"],
-    zh: ["zh-Hans"],
-    "zh-CN": ["zh-Hans"],
+    default: ["zh-CN"],
+    zh: ["zh-CN"],
   },
+  supportedLngs: ["zh-CN", "en"],
   interpolation: {
     escapeValue: false, // react already safes from xss
   },
