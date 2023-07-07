a35414788632bb082601f4fccee96e39d22e6603
liuzhaowei55 <liuzhaowei55@gmail.com>
2023-07-07 17:38:01 +0800

    feat: add FormRule type

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,32 +1,37 @@
 import { LoginFormPage, ProFormText } from '@ant-design/pro-components';
-import { InternalFieldProps } from 'rc-field-form/lib/Field';
+import type { FormRule } from 'antd';
+import { Form } from 'antd';
 import { FC } from 'react';
 
-import { LoginRequest, login } from '@/services/LoginService.ts';
+import { LoginRequest, login } from '@/services/LoginService';
 
-const usernameFieldRules: InternalFieldProps['rules'] = [
+const usernameFieldRules: FormRule[] = [
   { required: true, message: '请输入用户名' },
   { min: 3, max: 16, message: '用户名长度在 3 ～ 16 位之间' },
 ];
-const passwordFieldRules: InternalFieldProps['rules'] = [
+const passwordFieldRules: FormRule[] = [
   { required: true, message: '请输入密码' },
   { min: 6, message: '密码长度最低为 6 位' },
 ];
 const LoginPage: FC = () => {
+  const [loginForm] = Form.useForm();
   const submitLoginRequest = async (formData: any) => {
     const request: LoginRequest = {
       username: formData.username,
       password: formData.password,
     };
-    login(request).then((response) => {
-      const data = response.data;
-      console.log(data);
-      return true;
-    });
-    return true;
+    login(request)
+      .then((response) => {
+        const data = response.data;
+        console.log(data);
+        return true;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   return (
-    <LoginFormPage title="登录" onFinish={submitLoginRequest}>
+    <LoginFormPage form={loginForm} title="登录" onFinish={submitLoginRequest}>
       <ProFormText
         name="username"
         label="用户名"
