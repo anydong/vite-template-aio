345f6625df758523121a77f2539555105d6cb3d1
liuzhaowei55 <liuzhaowei55@gmail.com>
2024-01-26 20:45:02 +0800

    feat: pkg upgrade

--- src/api/LoginApi.ts
+++ src/api/LoginApi.ts
@@ -1,7 +1,7 @@
-import type { FormRule } from 'antd';
+import type { FormRule } from "antd";
+import type { AxiosResponse } from "axios";
 
-import client from '@/libs/request';
-import { AxiosResponse } from 'axios';
+import client from "@/libs/request";
 
 export interface LoginRequest {
   username: string;
@@ -10,15 +10,15 @@ export interface LoginRequest {
 
 export const LoginRequestFormRule: Record<keyof LoginRequest, FormRule[]> = {
   username: [
-    { required: true, message: '请输入用户名' },
-    { min: 3, max: 16, message: '用户名长度在 3 ～ 16 位之间' },
+    { required: true, message: "请输入用户名" },
+    { min: 3, max: 16, message: "用户名长度在 3 ～ 16 位之间" },
   ],
   password: [
-    { required: true, message: '请输入密码' },
-    { min: 6, message: '密码长度最低为 6 位' },
+    { required: true, message: "请输入密码" },
+    { min: 6, message: "密码长度最低为 6 位" },
   ],
 };
 
 export const login = async (request: LoginRequest): Promise<AxiosResponse<Response>> => {
-  return client.post('/auth/login', request);
+  return client.post("/auth/login", request);
 };
