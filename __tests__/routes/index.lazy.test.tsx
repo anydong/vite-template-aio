123e85bc6e14c1717926b8a6fad3b1bc772f02a9
liuzhaowei55 <liuzhaowei55@gmail.com>
2024-06-14 22:32:23 +0800

    feat: add vitest

--- __tests__/routes/index.lazy.test.tsx
+++ __tests__/routes/index.lazy.test.tsx
@@ -1,18 +1,19 @@
-import { it, describe } from "vitest";
+import { it, describe, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { createRouter, RouterProvider, createMemoryHistory } from "@tanstack/react-router";
 
 import { routeTree } from "../../src/routeTree.gen";
 
 describe("routes", () => {
-  it("/", async ({ expect }) => {
+  test("/", async ({ expect }) => {
     const memoryHistory = createMemoryHistory({
       initialEntries: ["/"], // Pass your initial url
     });
 
     const router = createRouter({ routeTree, history: memoryHistory });
     render(<RouterProvider router={router} />);
-    const element = screen.getByRole("heading", { level: 1 });
-    expect(element).toBeDefined();
+    console.log(screen.findByText("div"));
+
+    expect(screen.getByRole("heading")).toBeDefined();
   });
 });
