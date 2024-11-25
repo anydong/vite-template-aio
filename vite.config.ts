ece69190502ae7b74f942e9abf459c22a44fd0ef
Your Name <you@example.com>
2024-11-25 10:56:48 +0800

    feat: update vite paths config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,13 @@
 /// <reference types="vitest" />
 
-import path from "node:path";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
+  plugins: [TanStackRouterVite(), react(), tsconfigPaths()],
   build: {
     rollupOptions: {
       output: {
