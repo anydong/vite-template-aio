052aea4ca94a1eb8f217abe85043819f16d4c112
liuzhaowei55 <liuzhaowei55@gmail.com>
2025-02-12 17:47:45 +0800

    feat: mix

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -6,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react(), tailwindcss(), tsconfigPaths()],
+  plugins: [TanStackRouterVite(), react(), tsconfigPaths()],
   build: {
     rollupOptions: {
       output: {
